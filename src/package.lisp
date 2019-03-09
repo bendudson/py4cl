e0ed03f4b3a4c395704e4e417d033c3b6bbcb97e
Ben Dudson <bd512@york.ac.uk>
2019-03-09 15:23:41 +0000

    Remove python-call* and export-function*
    Don't properly support multiple python instances (yet), so remove
    these variants which take the process as first argument.
    
    Also ensure that test script is in package :cl-user

--- src/package.lisp
+++ src/package.lisp
@@ -11,12 +11,10 @@
    #:python-start-if-not-alive
    #:python-eval
    #:python-exec
-   #:python-call*
    #:python-call
    #:python-call-async
    #:import-function
    #:import-module
-   #:export-function*
    #:export-function
    #:python-setf
    #:python-version-info))
