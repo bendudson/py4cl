39408de7695e0139d3687bda2a6b17ee25ff12f8
digikar99 <shubhamkarayare@gmail.com>
2019-06-28 16:09:35 +0530

    defpyfun: import-module and as
    import-module: to take care of name conflicts
    as: to provide support for "from module import function as func"

--- src/import-export.lisp
+++ src/import-export.lisp
@@ -17,9 +17,23 @@
           (collect (char-downcase ch) into out-string result-type string))
         (finally (return (string-upcase out-string)))))
 
+;; In essence, this macro should give the full power of the
+;;   "from modulename import function as func"
+;; to the user.
+
+;; "from keras.layers import Input" creates only "Input" and not
+;; "keras.layers.Input" in python;
+;; However, this leaves open the chance of a name conflict
+;; - what if two python modules have the same name?
+;; import-module takes care of this, along with keeping minimal work
+;; in defpymodule
+
 (defmacro defpyfun (fun-name pymodule-name
-                           &key (lisp-function (lispify-name fun-name))
-                             (lisp-package *package*))
+                    &key
+                      (as fun-name)
+                      (import-module nil) ; see above
+                      (lisp-fun-name (lispify-name as))
+                      (lisp-package *package*))
   "Defines a function which calls python
 Example
   (py4cl:pyexec \"import math\")
@@ -27,55 +41,58 @@ Example
   (math.sqrt 42) -> 6.4807405
 
 Keywords:
-  LISP-FUNCTION is a string, denoting the symbol to which the function is assigned.
+  LISP-FUN-NAME is a string, denoting the symbol to which the function is assigned.
   DOCSTRING is a string which becomes the function docstring
   FROM specifies a pymodule to load the function from. This will cause the python
     pymodule to be imported into the python session.
 "
   (check-type fun-name string)
-  (check-type lisp-function string)
+  (check-type lisp-fun-name string)
   (check-type lisp-package package)
-  (when pymodule-name
-    (python-start-if-not-alive)
-    (check-type pymodule-name string)   
-    (pyexec "from " pymodule-name " import " fun-name))
-  
-  (let ((fun-symbol (intern lisp-function lisp-package))
-        (fullname (concatenate 'string pymodule-name "." fun-name))
-        (fun-doc (pyeval pymodule-name "." fun-name ".__doc__")))
+  (check-type pymodule-name string)
+  (check-type as string)
+  (python-start-if-not-alive)
+  (if import-module
+      (pyexec "import " pymodule-name)
+      (pyexec "from " pymodule-name " import " fun-name " as " as))
+  (let* ((fun-symbol (intern lisp-fun-name lisp-package))
+         (fullname (if import-module
+                       (concatenate 'string pymodule-name "." fun-name)
+                       fun-name))
+         (fun-doc (pyeval fullname ".__doc__")))
     (if (member
-           (slot-value (pyeval fullname) 'type)
-           '("<class 'function'>") ;;  "<class 'builtin_function_or_method'>"
-           :test 'string=)
-          (let* ((fun-args ;; includes all the local variables
-                  (mapcar-> (pyeval fullname ".__code__.co_varnames")
-                            #'lispify-name
-                            #'intern))
-                 (fun-argcount ;; to exclude the local variables
-                  (pyeval fullname ".__code__.co_argcount"))
-                 (arg-list (subseq fun-args 0 fun-argcount)))
-            `(progn
-               (defun ,fun-symbol (&key ,@arg-list)
-                 ,(or fun-doc "Python function")
-                 (funcall #'pycall ,fun-name ,@arg-list))
-               (export ',fun-symbol ,lisp-package)))
+         (slot-value (pyeval fullname) 'type)
+         '("<class 'function'>") ;;  "<class 'builtin_function_or_method'>"
+         :test 'string=)
+        (let* ((fun-args ;; includes all the local variables
+                (mapcar-> (pyeval fullname ".__code__.co_varnames")
+                          #'lispify-name
+                          #'intern))
+               (fun-argcount ;; to exclude the local variables
+                (pyeval fullname ".__code__.co_argcount"))
+               (arg-list (subseq fun-args 0 fun-argcount)))
           `(progn
-             (defun ,fun-symbol (&rest args)
-                    ,(or fun-doc "Python function")
-                    (apply #'pycall ,fun-name args))
-             (export ',fun-symbol ,lisp-package)))))
+             (defun ,fun-symbol (&key ,@arg-list)
+               ,(or fun-doc "Python function")
+               (funcall #'pycall ,fullname ,@arg-list))
+             (export ',fun-symbol ,lisp-package)))
+        `(progn
+           (defun ,fun-symbol (&rest args)
+             ,(or fun-doc "Python function")
+             (apply #'pycall ,fullname args))
+           (export ',fun-symbol ,lisp-package)))))
 
 
 (defmacro defpysubmodules (pymodule-name)
-    (let ((submodules
-           (py4cl:pyeval "[(modname, ispkg) for importer, modname, ispkg in "
-                              "pkgutil.iter_modules("
-                              pymodule-name
-                              ".__path__)]")))
-      (iter (for (submodule has-submodules) in-vector submodules)
-            (collect `(defpymodule ,(concatenate 'string
-                                                   pymodule-name "." submodule)
-                                     ,has-submodules)))))
+  (let ((submodules
+         (py4cl:pyeval "[(modname, ispkg) for importer, modname, ispkg in "
+                       "pkgutil.iter_modules("
+                       pymodule-name
+                       ".__path__)]")))
+    (iter (for (submodule has-submodules) in-vector submodules)
+          (collect `(defpymodule ,(concatenate 'string
+                                               pymodule-name "." submodule)
+                        ,has-submodules)))))
 
 
 
@@ -112,8 +129,8 @@ is NIL."
   
   ;; fn-names  All callables whose names don't start with "_" 
   (let ((fun-names (pyeval "[name for name, fn in inspect.getmembers("
-                                 pymodule-name
-                                 ", callable) if name[0] != '_']"))
+                           pymodule-name
+                           ", callable) if name[0] != '_']"))
         ;; Get the package name by passing through reader, rather than using STRING-UPCASE
         ;; so that the result reflects changes to the readtable
         ;; Setting *package* causes symbols to be interned by READ-FROM-STRING in this package
@@ -126,16 +143,15 @@ is NIL."
             ;; (format t "Submodules imported!~%")
             (iter (for fun-name in-vector fun-names)
                   (collect (macroexpand `(defpyfun
-                                          ,fun-name ,pymodule-name
-                                          :lisp-package ,exporting-package)))))))
-
-
+                                             ,fun-name ,pymodule-name
+                                             :lisp-package ,exporting-package
+                                             :import-module t)))))))
 
 (defun export-function (function python-name)
   "Makes a lisp FUNCTION available in python process as PYTHON-NAME"
   (pyexec (concatenate 'string
-                            python-name
-                            "=_py4cl_LispCallbackObject("
-                            (write-to-string
-                             (object-handle function))
-                            ")")))
+                       python-name
+                       "=_py4cl_LispCallbackObject("
+                       (write-to-string
+                        (object-handle function))
+                       ")")))
