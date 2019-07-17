261a7b22ae27fafaad1a5f6cc91f1df2073d5374
digikar99 <shubhamkarayare@gmail.com>
2019-07-17 22:01:14 +0530

    bugfix: pymethod-monitor was "read-from-string"ing

--- src/callpython.lisp
+++ src/callpython.lisp
@@ -237,9 +237,8 @@ functions like keras.Model.fit."
 (defun pymethod-monitor (obj method-name arg-list &key (interval 1) (output *standard-output*))
   "Same as PYCALL-MONITOR, but handy for calling methods."
   (apply #'pycall-monitor
-	 (read-from-string
-          (concatenate 'string "|" (pythonize obj)
-                       "." (pythonize-name method-name) "|"))
+	 (concatenate 'string (pythonize obj)
+                       "." (pythonize-name method-name))
          (cons arg-list
                `(:interval ,interval :output ,output))))
 
